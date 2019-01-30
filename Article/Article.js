bda03b595af75039c412aa3ce0062b07846b8850
Patrick <patrick.schwindt14@gmail.com>
2019-01-30 15:49:02 -0700

    MVP Complete

--- Article/Article.js
+++ Article/Article.js
@@ -22,7 +22,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.add('article-open');
+    this.domElement.classList.toggle('article-open');
     //console.log(this.expandButton);
   }
 }
