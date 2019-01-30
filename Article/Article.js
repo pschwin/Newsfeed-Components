af5924cd9bd6ff711abccf82bb98747be229b06a
Patrick <patrick.schwindt14@gmail.com>
2019-01-30 15:40:22 -0700

    Menu and Article Updates

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,27 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent += 'Expand';
     
+    console.log(this.domElement);
+    //console.log(this.expandButton);
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    //Implicit Binding
+    //ES5
+    //this.expandButton.addEventListener('click', this.expandButton.bind(this));
+    //ES6
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.add('article-open');
+    //console.log(this.expandButton);
   }
 }
 
